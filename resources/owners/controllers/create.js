b5934b2e0b58aa954c5a49a83e9e65d99116ba09
Arun Sood <arsood@gmail.com>
2017-12-12 15:15:15 -0800

    Initial commit

---
+++ resources/owners/controllers/create.js
@@ -0,0 +1,27 @@
+// MongoDB connection
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const mongoUrl = "mongodb://localhost:27017/pet_list_db";
+
+module.exports = (req, res) => {
+  // Insert a record into the "owners" collection using the data from the form
+  // You can use the data from the form below:
+
+  const newOwner = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber
+  }
+
+  // Call this method when the query is complete: res.redirect("/");
+
+  MongoClient.connect(mongoUrl, (err, db) => {
+    db
+      .collection("owners")
+      .insert(newOwner, (err, newOwner) => {
+        res.redirect("/");
+        db.close();
+      });
+  });
+}
---
+++ resources/owners/controllers/index.js
@@ -0,0 +1,27 @@
+// MongoDB connection
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const mongoUrl = "mongodb://localhost:27017/pet_list_db";
+
+module.exports = (req, res) => {
+  // Find all owners from the "owners" collection here
+  // Aggregate them using the $lookup operator to join their associated pets
+  // Call the method below, passing in the owners array:
+
+  // res.render("index", {
+  //   owners: ownersArray
+  // });
+  MongoClient.connect(mongoUrl, (err, db) => {
+    db
+      .collection("owners")
+      .find({})
+      .toArray((err, owners) => {
+        // Render the template
+        res.render("index", {
+          owners: owners
+        });
+
+        db.close();
+      });
+  });
+}
